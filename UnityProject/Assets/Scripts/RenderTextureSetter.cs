294ac85ccb0adf573edcd9a67ddd783185475e60
david-a-diaz <david.diaz@jojugames.com>
2017-07-17 16:28:48 -0500

    Standard assets added and cameras setup

---
+++ UnityProject/Assets/Scripts/RenderTextureSetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTextureSetter : MonoBehaviour 
+{
+    public Camera TNECamera;
+    public RenderTexture TNETexture;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        TNETexture.width = Screen.width;
+        TNETexture.height = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
